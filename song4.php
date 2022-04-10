e097dddd59af582eb202bf9410fd0abf2c704009
BlancoJohncarlo <blancojhoncarlo2@gmail.com>
2022-04-11 00:30:18 +0800

    Song 4 Php Finish

---
+++ song4.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Song4-Love You Once-Clara Mae</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> Music Lyics </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to Music Lyrics</h1>
+                <p class="lead">With music we can be free</p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        <?php
+                        echo "<h2 class='text-primary'>Love You Once</h2>";
+                        echo "<p class='lead'>Clara Mae</p>";
+
+                        echo "<p>Back here in the same town, same bar</p>";
+                        echo "<p>But now I don't even know you</p>";
+                        echo "<p>It's like we look down, ignore</p>";
+                        echo "<p>Now nothing's like it used to</p>";
+                        echo "<br/>";
+
+                        echo "<p>Getting heavy to breathe in this room together</p>";
+                        echo "<p>It's so awkward, we can't seem to do it better</p>";
+                        echo "<p>Can't we just fake a smile, put our shit to the side?</p>";
+                        echo "<br/>";
+
+
+                        echo "<p>We don't need to be best friends</p>";
+                        echo "<p>We don't need to hang again</p>";
+                        echo "<p>But tell me why we have to be strangers?</p>";
+                        echo "<p>'Cause I loved you once</p>";
+                        echo "<br/>";
+
+                        echo "<p>We don't need to act the same</p>";
+                        echo "<p>But you act like you don't know my name</p>";
+                        echo "<p>Tell me why we have to be strangers?</p>";
+                        echo "<p>'Cause I loved you once</p>";
+                        echo "<br/>";
+
+
+                        echo "<p>What would you say if I</p>";
+                        echo "<p>If I come over</p>";
+                        echo "<p>And stand face to face now</p>";
+                        echo "<p>Now that we're older</p>";
+                        echo "<br/>";
+
+                        echo "<p>Getting heavy to breathe in this room together</p>";
+                        echo "<p>It's so awkward, we can't seem to do it better</p>";
+                        echo "<p>Can't we just fake a smile, put our shit to the side?</p>";
+                        echo "<br/>";
+
+
+                        echo "<p>We don't need to be best friends</p>";
+                        echo "<p>We don't need to hang again</p>";
+                        echo "<p>But tell me why we have to be strangers?</p>";
+                        echo "<p>'Cause I loved you once</p>";
+                        echo "<br/>";
+
+                        echo "<p>We don't need to act the same</p>";
+                        echo "<p>But you act like you don't know my name</p>";
+                        echo "<p>Tell me why we have to be strangers?</p>";
+                        echo "<p>'Cause I loved you once</p>";
+                        echo "<br/>";
+
+                        echo "<p>Thinking after all this time</p>";
+                        echo "<p>I just wanna meet your eyes</p>";
+                        echo "<p>So I can remember why</p>";
+                        echo "<p>Why I loved you once</p>";
+                        echo "<br/>";
+
+                        echo "<p>Thinking after all this time</p>";
+                        echo "<p>We should leave it all behind</p>";
+                        echo "<p>So I can remember why</p>";
+                        echo "<p>Why I loved you once</p>";
+                        echo "<br/>";
+
+                        echo "<p>We don't need to be best friends</p>";
+                        echo "<p>We don't need to hang again</p>";
+                        echo "<p>Tell me why we have to be strangers?</p>";
+                        echo "<p>'Cause I loved you once</p>";
+                        echo "<br/>";
+
+ 
+                        ?>
+
+                        Back to main page <a href = "index.php">here!</a>
+                    
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. John Carlo M. Blanco</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+        
+
+    
+    </body>
+</html>
