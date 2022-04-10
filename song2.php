bad3c48f9292ce73ed679e7f8a59f9a7657297fd
BlancoJohncarlo <blancojhoncarlo2@gmail.com>
2022-04-10 23:47:52 +0800

    Song 2 Php Finish

---
+++ song2.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Song2-Iba-Zack Tabudlo</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> Music Lyics </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to Music Lyrics</h1>
+                <p class="lead">With music we can be free</p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        <?php
+                        echo "<h2 class='text-primary'>Iba</h2>";
+                        echo "<p class='lead'>Zack Tabudlo</p>";
+
+                        echo "<p>Na-na (hey)</p>";
+                        echo "<p>Na-na (hey)</p>";
+                        echo "<p>Uh-uh, uh-uh</p>";
+                        echo "<br/>";
+
+                        echo "<p>Hindi ko ba nasabi sa 'yo</p>";
+                        echo "<p>Mga kailangan kong itago</p>";
+                        echo "<p>No'ng panahon na lagi kong nakikita</p>";
+                        echo "<p>Na iba'ng ngiti mo?</p>";
+                        echo "<p>Nahulog ka na ba sa iba?</p>";
+                        echo "<p>Naghihintay lang ba ako sa wala?</p>";
+                        echo "<p>Wala naman palang tumatalab</p>";
+                        echo "<p>Sa ginawa ko para sa 'ting dalawa</p>";
+                        echo "<br/>";
+
+                        echo "<p>Alam ko na hindi na tayo</p>";
+                        echo "<p>Pero ba't pinapamukha mo?</p>";
+                        echo "<p>Di pa ba sapat lahat ng ginawa ko?</p>";
+                        echo "<p>Nagkulang ba sa 'kin o sa 'yo?</p>";
+                        echo "<p>May pakiramdam din naman ako</p>";
+                        echo "<p>Wag ka naman parang gago</p>";
+                        echo "<p>Ako pa lumalabas na masama</p>";
+                        echo "<p>Minahal naman kita nang buong-buo</p>";
+                        echo "<br/>";
+
+                        echo "<p>Alam ko naman, alam ko naman</p>";
+                        echo "<p>Hindi ako ang hanap mo na, ang hanap mo na</p>";
+                        echo "<p>Wag mo na ngang patagalin pa, patagalin pa</p>";
+                        echo "<p>Kung wala namang nangyayari na, nangyayari na</p>";
+                        echo "<p>Nagmumukha na lang akong iba</p>";
+                        echo "<p>Sa dapat mundo nating dalawa</p>";
+                        echo "<p>Sa'n na nga ba tayo pupunta?</p>";
+                        echo "<p>Tinadhana naman, pero naghahanap ka ng iba</p>";
+                        echo "<br/>";
+
+                        echo "<p>Ba-ba, ba, ng iba (ng iba)</p>";
+                        echo "<p>Ba-ba, ba, ng iba (ng iba)</p>";
+                        echo "<p>Ba-ba, ba, ng iba (ng iba)</p>";
+                        echo "<p>Ba, 'di ko alam, ba't naghanap ng iba?</p>";
+                        echo "<p>Ba-ba, ba, ng iba</p>";
+                        echo "<p>Ba-ba, ba, ng iba (iba)</p>";
+                        echo "<p>Ba, yeah, yeah, bakit ba?</p>";
+                        echo "<p>Bakit ba naghanap pa ng iba?</p>";
+                        echo "<br/>";
+
+                        echo "<p>Binalikan ko ang mga litrato</p>";
+                        echo "<p>Panahon na masaya pa tayo</p>";
+                        echo "<p>Hinahanap ko'ng mga ngiti mo</p>";
+                        echo "<p>Mukha namang totoo</p>";
+                        echo "<p>Pero ba't bigla kang nagbago?</p>";
+                        echo "<p>Nawala 'yung kinang sa mga mata mo</p>";
+                        echo "<p>Di naman siguro peke lahat ng ito</p>";
+                        echo "<p>Pero bakit ganito?</p>";
+                        echo "<br/>";
+
+
+                        echo "<p>Nawawala na 'ata ako sa sarili</p>";
+                        echo "<p>Binigay ko ang lahat para sa pag-ibig</p>";
+                        echo "<p>Hindi ko na alam ang aking gagawin (gagawin)</p>";
+                        echo "<p>Sinira mo na 'ko sa iba</p>";
+                        echo "<p>Ako na 'yung nagmumukhang masama</p>";
+                        echo "<p>Pero ba't nandito ako nagdurusa?</p>";
+                        echo "<br/>";
+
+                        echo "<p>Alam ko naman, alam ko naman</p>";
+                        echo "<p>Hindi ako ang hanap mo na, ang hanap mo na</p>";
+                        echo "<p>'Wag mo na ngang patagalin pa, patagalin pa (yeah, yeah, whoa)</p>";
+                        echo "<p>Kung wala namang nangyayari na, nangyayari na (ayaw ko na, ayaw ko na)</p>";
+                        echo "<p>Nagmumukha na lang akong iba (nagmumukha na lang iba)</p>";
+                        echo "<p>Sa dapat mundo nating dalawa</p>";
+                        echo "<p>Sa'n na nga ba tayo pupunta? (Sa'n na pupunta?)</p>";
+                        echo "<p>Tinadhana naman, pero naghahanap ka ng iba</p>";
+                        echo "<br/>";
+
+                        echo "<p>Ba-ba, ba, ng iba (ng iba)</p>";
+                        echo "<p>Ba-ba, ba, ng iba (ng iba)</p>";
+                        echo "<p>Ba-ba, ba, ng iba (ng iba, yeah)</p>";
+                        echo "<p>Nagmumukha na lang akong iba</p>";
+                        echo "<br/>";
+
+                        echo "<p>Sa dapat mundo nating dalawa (sa dapat mundo nating dalawa)</p>";
+                        echo "<p>Ano ba ang nangyari na? (Parang gago lang, 'di ba?)</p>";
+                        echo "<p>Tinadhana naman, pero naghahanap ka ng iba.</p>";
+                        echo "<br/>";
+
+
+                        
+ 
+                        ?>
+
+                        Back to main page <a href = "index.php">here!</a>
+                    
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. John Carlo M. Blanco</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+        
+
+    
+    </body>
+</html>
