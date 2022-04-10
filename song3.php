62006144ba078b5b181dcc827c50079c4359a605
BlancoJohncarlo <blancojhoncarlo2@gmail.com>
2022-04-11 00:10:21 +0800

    Song 3 Php Finish

---
+++ song3.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Song3-Off My Face-Justin Bieber</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> Music Lyics </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to Music Lyrics</h1>
+                <p class="lead">With music we can be free</p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        <?php
+                        echo "<h2 class='text-primary'>Off My Face</h2>";
+                        echo "<p class='lead'>Justin Bieber</p>";
+
+                        echo "<p>One touch and you got me stoned</p>";
+                        echo "<p>Higher than I've ever known</p>";
+                        echo "<p>You call the shots and I follow</p>";
+                        echo "<p>Sunrise, but the night still young</p>";
+                        echo "<p>No words, but we speak in tongues</p>";
+                        echo "<p>If you let me, I might say too much</p>";
+                        echo "<br/>";
+
+                        echo "<p>Your touch blurred my vision</p>";
+                        echo "<p>It's your world and I'm just in it</p>";
+                        echo "<p>Even sober I'm not thinkin' straight</p>";
+                        echo "<br/>";
+
+                        echo "<p>'Cause I'm off my face, in love with you</p>";
+                        echo "<p>I'm out my head, so into you</p>";
+                        echo "<p>And I don't know how you do it</p>";
+                        echo "<p>But I'm forever ruined by you, ooh-ooh-ooh</p>";
+                        echo "<br/>";
+
+
+                        echo "<p>Can't sleep 'cause I'm way too buzzed</p>";
+                        echo "<p>Too late, now you're in my blood</p>";
+                        echo "<p>I don't hate the way you keep me up</p>";
+                        echo "<br/>";
+
+
+
+                        echo "<p>Your touch blurred my vision</p>";
+                        echo "<p>It's your world and I'm just in it</p>";
+                        echo "<p>Even sober I'm not thinkin' straight</p>";
+                        echo "<br/>";
+
+                        echo "<p>'Cause I'm off my face, in love with you</p>";
+                        echo "<p>I'm out my head, so into you</p>";
+                        echo "<p>And I don't know how you do it</p>";
+                        echo "<p>But I'm forever ruined by you, ooh-ooh-ooh</p>";
+                        echo "<br/>";
+
+
+                        echo "<p>Ooh, ooh-ooh, ooh-ooh-ooh-ooh</p>";
+                        echo "<p>Ooh-ooh-ooh</p>";
+                        echo "<p>Ooh, ooh-ooh, ooh-ooh-ooh-ooh</p>";
+                        echo "<p>Ooh-ooh-ooh</p>";
+                        echo "<br/>";
+
+
+                        echo "<p>'Cause I'm off my face, in love with you</p>";
+                        echo "<p>I'm out my head, so into you</p>";
+                        echo "<p>And I don't know how you do it</p>";
+                        echo "<p>But I'm forever ruined by you, ooh-ooh-ooh</p>";
+                        echo "<br/>";
+
+
+                        echo "<p>By you, ooh-ooh-ooh</p>";
+                        echo "<br/>";
+                        
+ 
+                        ?>
+
+                        Back to main page <a href = "index.php">here!</a>
+                    
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. John Carlo M. Blanco</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+        
+
+    
+    </body>
+</html>
