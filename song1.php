2b6748656135239ecbafa52ac06ed453b881f2d5
BlancoJohncarlo <blancojhoncarlo2@gmail.com>
2022-04-10 23:11:55 +0800

    Song 1 Php Finish

---
+++ song1.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Song1-Anyone-Justin Bieber</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> Music Lyics </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to Music Lyrics</h1>
+                <p class="lead">With music we can be free</p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        <?php
+                        echo "<h2 class='text-primary'>Anyone</h2>";
+                        echo "<p class='lead'>Justin Bieber</p>";
+
+                        echo "<p>Dance with me under the diamonds</p>";
+                        echo "<p>See me like breath in the cold</p>";
+                        echo "<p>Sleep with me here in the silence</p>";
+                        echo "<p>Come kiss me, silver and gold</p>";
+                        echo "<br/>";
+
+                        echo "<p>You say that I won't lose you</p>";
+                        echo "<p>But you can't predict the future</p>";
+                        echo "<p>So, just hold on like you will never let go</p>";
+                        echo "<p>Yeah, if you ever move on without me</p>";
+                        echo "<p>I need to make sure you know that</p>";
+                        echo "<br/>";
+
+                        echo "<p>You are the only one I'll ever love</p>";
+                        echo "<p>(I gotta tell ya, gotta tell ya)</p>";
+                        echo "<p>Yeah, you, if it's not you, it's not anyone</p>";
+                        echo "<p>(I gotta tell ya, gotta tell ya)</p>";
+                        echo "<p>Looking back on my life</p>";
+                        echo "<p>You're the only good I've ever done (ever done)</p>";
+                        echo "<p>Yeah, you, if it's not you, it's not anyone (anyone)</p>";
+                        echo "<p>Not anyone</p>";
+                        echo"<br/>";
+
+                        echo "<p>Forever's not enough time to (oh)</p>";
+                        echo "<p>Love you the way that I want (love you the way that I want)</p>";
+                        echo "<p>'Cause every morning I find you (oh)</p>";
+                        echo "<p>I fear the day that I don't</p>";
+                        echo"<br/>";
+
+                        echo "<p>You say that I won't lose you</p>";
+                        echo "<p>But you can't predict the future</p>";
+                        echo "<p>'Cause certain things are out of our control</p>";
+                        echo "<p>Yeah, if you ever move on without me</p>";
+                        echo "<p>I need to make sure you know that</p>";
+                        echo "<br/>";
+
+                        echo "<p>You are the only one I'll ever love</p>";
+                        echo "<p>Only one (I gotta tell ya, gotta tell ya)</p>";
+                        echo "<p>Yeah, you, if it's not you it's not anyone</p>";
+                        echo "<p>(I gotta tell ya, gotta tell ya)</p>";
+                        echo "<p>Looking back on my life</p>";
+                        echo "<p>You're the only good I've ever done (I've ever done)</p>";
+                        echo "<p>Yeah, you, if it's not you, it's not anyone<p>";
+                        echo "<p>It's not anyone, not anyone</p>";
+                        echo "<br/>";
+
+                        echo "<p>Oh, oh, oh, oh</p>";
+                        echo "<p>If it's not you, it's not anyone</p>";
+                        echo "<p>Oh, oh, oh, yeah, whoa</p>";
+                        echo "<br/>";
+
+                        echo "<p>Yeah, you are the only one I'll ever love</p>";
+                        echo "<p>(I gotta tell ya, gotta tell ya)</p>";
+                        echo "<p>Yeah, you, if it's not you, it's not anyone</p>";
+                        echo "<p>(I gotta tell ya, gotta tell ya) gotta tell ya</p>";
+                        echo "<p>Looking back on my life</p>";
+                        echo "<p>You're the only good I've ever done (ever done, oh, yeah)</p>";
+                        echo "<p>Yeah, you, if it's not you, it's not anyone</p>";
+                        echo "<br/>";
+ 
+                        ?>
+
+                        Back to main page <a href = "index.php">here!</a>
+                    
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. John Carlo M. Blanco</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+        
+
+    
+    </body>
+</html>
