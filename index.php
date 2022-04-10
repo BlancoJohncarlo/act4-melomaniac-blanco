c1154e643a158691f7306882968bbb0dfaa1ee92
BlancoJohncarlo <blancojhoncarlo2@gmail.com>
2022-04-10 21:39:44 +0800

    Index Php Finish

---
+++ index.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> Music Lyics </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="song4.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="song5.php">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to Music Lyrics</h1>
+                <p class="lead">With music we can be free</p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        <?php
+                        echo "<h2 class='text-primary'>Ligh Switch</h2>";
+                        echo "<p class='lead'>Charlie Puth</p>";
+
+                        echo "<p>Why you callin' at 11:30</p>";
+                        echo "<p>When you only wanna do me dirty?</p>";
+                        echo "<p>But I hit right back</p>";
+                        echo "<p>'Cause you got that-that, yeah</p>";
+                        echo "<p>Why you always wanna act like lovers?</p>";
+                        echo "<p>But you never wanna be each others'</p>";
+                        echo "<p>I say Don't lool back</p>";
+                        echo "<p>But I go right back and</p>";
+                        echo "<br/>";
+
+                        echo "<p>All of sudden , I'm hypnotized</p>";
+                        echo "<p>You're the one that I can't deny</p>";
+                        echo "<p>Every time that I say I'm gonna walk away</p>";
+                        echo "<br/>";
+
+                        echo "<p>You turn me on like a light switch</p>";
+                        echo "<p>When you're moving your body around and around</p>";
+                        echo "<p>Now, I don't wanna fight this(No)</p>";
+                        echo "<p>You know how to just make me want</p>";
+                        echo "<p>You turn me on like a light switch</p>";
+                        echo "<p>When you're moving your body around and around</p>";
+                        echo "<p>You got me in a tight grip (Yeah)</p>";
+                        echo "<p>You know how to just make me want you, baby</p>";
+                        echo"<br/>";
+
+                        echo "<p>Do you love it when you keep me guessin'? (Me guessin')</p>";
+                        echo "<p>When you leave and then you leave me stressin'?(me stressin')</p>";
+                        echo "<p>But I can't stay mad when you walk like that, no</p>";
+                        echo "<p>Why you always wanna act like lovers?</p>";
+                        echo "<p>But you never wanna be each other's</p>";
+                        echo "<p>I say don't look back</p>";
+                        echo "<p>But I go right back and</p>";
+                        echo"<br/>";
+
+                        echo "<p>All of a sudden, I'm hypnotized (hypnotized)</p>";
+                        echo "<p>You're the one that I can't deny (can't deny)</p>";
+                        echo "<p>Every time that I say I'm gonna walk away (yeah)</p>";
+                        echo "<br/>";
+
+                        echo "<p>You turn me on like a light switch</p>";
+                        echo "<p>When you're movin' your body around and around</p>";
+                        echo "<p>Now, I don't wanna fight this (no)</p>";
+                        echo "<p>You know how to just make me want</p>";
+                        echo "<p>You turn me on like a light switch (switch)</p>";
+                        echo "<p>When you're movin' your body around and around</p>";
+                        echo "<p>You got me in a tight grip (grip)</p>";
+                        echo "<p>You know how to just make me want you, baby</p>";
+                        echo "<br/>";
+
+                        echo "<p>C'mon, c'mon, c'mon, c'mon
+C'mon</p>";
+                        echo "<p>and show me how you do (how you do)</p>";
+                        echo "<p>You want, you want, you want, you want
+You wanna</p>";
+                        echo "<p>keep me wantin' you (me wantin' you, girl)</p>";
+                        echo "<p>C'mon, c'mon, c'mon, c'mon
+C'mon</p>";
+                        echo "<p>and show me how you do (how you do)</p>";
+                        echo "<p>You want, you want, you want, you want</p>";
+                        echo "<p>You wanna keep me wantin' you</p>";
+                        echo "<br/>";
+
+                        echo "<p>You turn me on like a light switch</p>";
+                        echo "<p>When you're movin' your body around and around</p>";
+                        echo "<p>Now, I don't wanna fight this</p>";
+                        echo "<p>You know how to just make me want to, uhh</p>";
+                        echo "<br/>";
+
+                        echo "<p>You turn me on like a light switch</p>";
+                        echo "<p>When you're movin' your body around and around</p>";
+                        echo "<p>You got me in a tight grip</p>";
+                        echo "<p>You know how to just make me want you, baby</p>";
+                        echo "<br/>";
+
+                        echo "<p>C'mon, c'mon, c'mon, c'mon
+C'mon</p>";
+                        echo "<p>and show me how you do (how you do)</p>";
+                        echo "<p>You want, you want, you want, you want</p>";
+                        echo "<p>You wanna keep me wantin' you (baby)</p>";
+                        echo "<br/>";
+
+                        echo "<p>C'mon, c'mon, c'mon, c'mon
+C'mon</p>";
+                        echo "<p>and show me how you do (how you do)</p>";
+                        echo "<p>You want, you want, you want, you want</p>";
+                        echo "<p>You wanna keep me wantin' you</p>";
+                        echo "<br/>";
+
+
+                        
+                        ?>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. John Carlo M. Blanco</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
