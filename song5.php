87ceb367ddfa60a74189dcb31e5389c40d95ef7b
BlancoJohncarlo <blancojhoncarlo2@gmail.com>
2022-04-11 00:55:27 +0800

    Song 5 Php Finish

---
+++ song5.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Song5-September-James Arthur</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> Music Lyics </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to Music Lyrics</h1>
+                <p class="lead">With music we can be free</p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        <?php
+                        echo "<h2 class='text-primary'>September</h2>";
+                        echo "<p class='lead'>James Arthur</p>";
+
+                        echo "<p>I remember when I met you just before September</p>";
+                        echo "<p>You were dancing in the street rocking that pink and leather</p>";
+                        echo "<p>Begged my friend for your number</p>";
+                        echo "<p>I bet you don't remember, that's how we began</p>";
+                        echo "<br/>";
+
+                        echo "<p>I pursued you for a year, I would've waited longer</p>";
+                        echo "<p>I knew ultimately it would make the feeling stronger</p>";
+                        echo "<p>That first time that I kissed you, I could look in the mirror</p>";
+                        echo "<p>And like who I was (I liked who I was)</p>";
+                        echo "<br/>";
+
+                        echo "<p>I could never wait for the weekends when you would come over</p>";
+                        echo "<p>'Cause that's when I'd see you again</p>";
+                        echo "<p>You're all my strength and my weakness</p>";
+                        echo "<p>You battle my demons, you're still undefeated</p>";
+                        echo "<br/>";
+
+                        echo "<p>Sometimes I get a little bit jaded</p>";
+                        echo "<p>Too much pressure just to make it</p>";
+                        echo "<p>You smile when I'm angry and I hate it</p>";
+                        echo "<p>But I'll still love you for the rest of my life</p>";
+                        echo "<p>My crush on you has never faded</p>";
+                        echo "<p>Let's go back to bed until we break it</p>";
+                        echo "<p>I'm gonna love you for the rest of my life</p>";
+                        echo "<br/>";
+
+
+                        echo "<p>Ooh</p>";
+                        echo "<p>If you wanted to</p>";
+                        echo "<p>I'd start a family with you</p>";
+                        echo "<br/>";
+
+
+                        echo "<p>I remember being foolish when I met your mother</p>";
+                        echo "<p>Because she had that Irish charm, right then I knew I love her</p>";
+                        echo "<p>That's where you got your heart when we get back to the car</p>";
+                        echo "<p>I knew you were the one (I knew you were the one)</p>";
+                        echo "<br/>";
+
+
+                        echo "<p>I could never wait for the weekends when you would come over</p>";
+                        echo "<p>'Cause that's when I'd see you again</p>";
+                        echo "<p>You're all my strength and my weakness</p>";
+                        echo "<p>You battle my demons, you're still undefeated</p>";
+                        echo "<br/>";
+
+
+                        echo "<p>Sometimes I get a little bit jaded</p>";
+                        echo "<p>Too much pressure just to make it</p>";
+                        echo "<p>You smile when I'm angry and I hate it</p>";
+                        echo "<p>But I'll still love you for the rest of my life</p>";
+                        echo "<p>My crush on you has never faded</p>";
+                        echo "<p>Let's go back to bed until we break it</p>";
+                        echo "<p>I'm gonna love you for the rest of my life</p>";
+                        echo "<br/>";
+
+
+                        echo "<p>Ooh</p>";
+                        echo "<p>If you wanted to</p>";
+                        echo "<p>I'd start a family with you</p>";
+                        echo "<p>Ooh</p>";
+                        echo "<p>If you wanted to</p>";
+                        echo "<p>I'd start a family with you</p>";
+                        echo "<br/>";
+
+
+                        echo "<p>Sometimes I get a little bit jaded</p>";
+                        echo "<p>Too much pressure just to make it</p>";
+                        echo "<p>You smile when I'm angry and I hate it</p>";
+                        echo "<p>But I'll still love you for the rest of my life</p>";
+                        echo "<p>My crush on you has never faded</p>";
+                        echo "<p>Let's go back to bed until we break it</p>";
+                        echo "<p>I'm gonna love you for the rest of my life</p>";
+                        echo "<br/>";
+
+                        echo "<p>Ooh</p>";
+                        echo "<p>If you wanted to</p>";
+                        echo "<p>I'd start a family with you</p>";
+                        echo "<p>Ooh</p>";
+                        echo "<p>If you wanted to</p>";
+                        echo "<p>I'd start a family with you</p>";
+                        echo "<br/>";
+ 
+                        ?>
+
+                        Back to main page <a href = "index.php">here!</a>
+                    
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. John Carlo M. Blanco</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+        
+
+    
+    </body>
+</html>
